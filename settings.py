f9a1dcbbe9c9f95d7b2cd6c7f775143660c377cf
Suhail Haris <sh.skyinfo@gmail.com>
2023-02-03 22:37:46 +0530

    Remove external dependencies + refactor
    - We can now run using only in built python functions.
    - Switch to ip-api.com from ipinfo.io
    - Optimize code and logic

--- settings.py
+++ settings.py
@@ -3,4 +3,3 @@ Settings/ENV
 """
 TELEGRAM_API_KEY = None
 TELEGRAM_CHAT_ROOM_ID = None
-IPINFO_KEY = None
--- ssh_alerts.py
+++ ssh_alerts.py
@@ -4,56 +4,52 @@ Just a Simple Python Telegram SSH Alert Script
 from datetime import datetime
 import os
 import sys
+import json
 import settings as env
-import requests
-import ipinfo
-
+import urllib.request, urllib.parse
 
 TELEGRAM_API_KEY = env.TELEGRAM_API_KEY
 TELEGRAM_CHAT_ROOM_ID = env.TELEGRAM_CHAT_ROOM_ID
 
-IPINFO_KEY = env.IPINFO_KEY
-
 HOST_IP = os.popen("hostname -I | awk '{ print $1 }'").readlines()[0].replace("\n", "")
 HOST_NAME = os.popen("hostname").readlines()[0].replace("\n", "")
 
+IP_API_SITE = "http://ip-api.com/json/"
 
 def main(ssh_ip):
     """The Main Function"""
+    details = None
 
-    if IPINFO_KEY is not None:
-        handler = ipinfo.getHandler(IPINFO_KEY)
-        details = handler.getDetails(ssh_ip).details
+    try:
+        details = urllib.request.urlopen(IP_API_SITE+ssh_ip)
+        details = details.read()
+        details = json.loads(details.decode("utf-8"))
+    except Exception as e:
+        print("Exception while trying to call API",e)
 
-        # print(details)
-
+    if details:
         message_to_send = f"""
         Hey,
-        We have a login from {details['ip']} at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
+        We have a login from {details['query']} at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
         Host Server: {HOST_IP} ({HOST_NAME})
 
         I've gone ahead and tracked the IP:
-        {details['ip']}
-        {details['city']}, {details['region']}, {details['country']}
-        {details['org']}
-        {details['country_name']}
+        {details['query']}
+        {details['city']}, {details['regionName']}, {details['country']}
+        {details['isp']} ({details['as']})
+        {details['country']}
         """
-
-    else:
+    else:    
         message_to_send = f"""
         Hey,
         We have a login from {ssh_ip} at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
         Host Server: {HOST_IP} ({HOST_NAME})
         """
 
-    # print(message_to_send)
+    message_to_send_encoded = urllib.parse.quote(message_to_send)
+    send_message = f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage?chat_id={TELEGRAM_CHAT_ROOM_ID}&text={message_to_send_encoded}"
 
-    send_message = f"""
-    https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage?chat_id={TELEGRAM_CHAT_ROOM_ID}&text={message_to_send}
-    """
-
-    # print(requests.get(send_message, timeout=10).json())
-    requests.get(send_message, timeout=10)
+    urllib.request.urlopen(send_message)
 
 
 main(ssh_ip=sys.argv[2])
